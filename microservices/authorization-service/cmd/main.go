f9ad64144ad8822a83d8d575b6d45429dfcbaa75
lvanic <Polzavatelyoutuba@gmail.com>
2023-04-15 16:17:32 +0300

    fix struct

--- microservices/authorization-service/cmd/main.go
+++ microservices/authorization-service/cmd/main.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"log"
-	// "net/http"
-	// "gorm.io/driver/mysql"
+
 	"github.com/gin-gonic/gin"
-	// "gorm.io/gorm"
 )
 
 func main() {
-	// dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local";
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{});
-
-	// if err != nil {
-	// 	db.First();
-	// }
 	router := gin.Default()
 
 	router.GET("/authorize", AuthorizeHandler)
